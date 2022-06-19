5129a9f17db28d042edd7b1b91db272403fea163
Stanislav Levchenko <staslevchenko@protonmail.com>
2022-06-19 22:23:03 +0300

    AppHeader component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import pageStyle from './App.module.css';
+import AppHeader from '../AppHeader/AppHeader';
+
+
+
+export default function App () {
+    return(
+        <div className={pageStyle.page}>
+          <AppHeader />
+            <main>
+
+
+            </main>
+        </div>
+    )
+}
